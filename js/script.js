858052105f2062b8f6e0ebb62d4896dc26a0d3a6
Alexeencokirill <97600028+Alexeencokirill@users.noreply.github.com>
2023-10-03 20:26:52 +0200

    tv-details

--- js/script.js
+++ js/script.js
@@ -39,7 +39,9 @@ async function displayPopularMovies() {
   });
 }
 
-// Display 20 most popular TV Shows
+
+
+// Display 20 most popular tv shows
 async function displayPopularShows() {
   const { results } = await fetchAPIData('tv/popular');
 
@@ -47,28 +49,27 @@ async function displayPopularShows() {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
-        <a href="movie-details.html?id=${show.id}">
-          ${
-            show.poster_path
-              ? `<img
+          <a href="tv-details.html?id=${show.id}">
+            ${
+              show.poster_path
+                ? `<img
               src="https://image.tmdb.org/t/p/w500${show.poster_path}"
               class="card-img-top"
               alt="${show.name}"
             />`
-              : `
-            <img
-            src="images/no-image.jpg"
+                : `<img
+            src="../images/no-image.jpg"
             class="card-img-top"
             alt="${show.name}"
           />`
-          }
-        </a>
-        <div class="card-body">
-          <h5 class="card-title">${show.name}</h5>
-          <p class="card-text">
-            <small class="text-muted">Air Date: ${show.first_air_date}</small>
-          </p>
-        </div>
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${show.first_air_date}</small>
+            </p>
+          </div>
         `;
 
     document.querySelector('#popular-shows').appendChild(div);
@@ -143,6 +144,77 @@ async function displayMovieDetails() {
         `;
 
   document.querySelector('#movie-details').appendChild(div);
+}
+
+
+// Display Show Details
+async function displayShowDetails() {
+  const showId = window.location.search.split('=')[1];
+
+  const show = await fetchAPIData(`tv/${showId}`);
+
+  // Overlay for background image
+  displayBackgroundImage('tv', show.backdrop_path);
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+  <div class="details-top">
+          <div>
+          ${
+            movie.poster_path
+              ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+              : `
+            <img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+          }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)}/ 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>${movie.overview}</p>
+            <h5>Genres</h5>
+            <ul class="list-group">${movie.genres
+              .map((genre) => `<li> ${genre.name} </li>`)
+              .join('')}</ul>
+            <a href="${
+              movie.homepage
+            }" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+              movie.budget
+            )}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+              movie.revenue
+            )}</li>
+            <li><span class="text-secondary">Runtime:</span> ${
+              movie.runtime
+            } minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies.map(
+            (company) => `
+          <span>${company.name}</span>`
+          ).join(', ')}</div>
+        </div>
+        `;
+
+  document.querySelector('#show-details').appendChild(div);
 }
 
 // Display Backdrop On Details Pages
@@ -220,12 +292,12 @@ function init() {
       displayPopularShows();
       break;
     
-    case '/movie-details.html?id=615656':
     case '/movie-details.html':
       displayMovieDetails();
       break;
 
     case '/tv-details.html':
+      displayShowDetails();
       console.log('TV Details');
       break;
 
