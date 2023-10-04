862892bf3f32a7ad9e9cd3593363dcfdb2b24a99
Alexeencokirill <97600028+Alexeencokirill@users.noreply.github.com>
2023-10-04 21:40:46 +0200

    tv-show-details

--- js/script.js
+++ js/script.js
@@ -161,55 +161,49 @@ async function displayShowDetails() {
   <div class="details-top">
           <div>
           ${
-            movie.poster_path
+            show.poster_path
               ? `<img
-              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
               class="card-img-top"
-              alt="${movie.title}"
+              alt="${show.name}"
             />`
               : `
             <img
             src="images/no-image.jpg"
             class="card-img-top"
-            alt="${movie.title}"
+            alt="${show.name}"
           />`
           }
           </div>
           <div>
-            <h2>${movie.title}</h2>
+            <h2>${show.name}</h2>
             <p>
               <i class="fas fa-star text-primary"></i>
-              ${movie.vote_average.toFixed(1)}/ 10
+              ${show.vote_average.toFixed(1)}/ 10
             </p>
-            <p class="text-muted">Release Date: ${movie.release_date}</p>
-            <p>${movie.overview}</p>
+            <p class="text-muted">Last Air Date: ${show.first_air_date
+            }</p>
+            <p>${show.overview}</p>
             <h5>Genres</h5>
-            <ul class="list-group">${movie.genres
+            <ul class="list-group">${show.genres
               .map((genre) => `<li> ${genre.name} </li>`)
               .join('')}</ul>
             <a href="${
-              movie.homepage
+              show.homepage
             }" target="_blank" class="btn">Visit Movie Homepage</a>
           </div>
         </div>
         <div class="details-bottom">
           <h2>Movie Info</h2>
           <ul>
-            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
-              movie.budget
-            )}</li>
-            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
-              movie.revenue
-            )}</li>
-            <li><span class="text-secondary">Runtime:</span> ${
-              movie.runtime
-            } minutes</li>
-            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+            <li><span class="text-secondary">Number of Episodes: </span>${show.number_of_episodes}</li>
+            <li><span class="text-secondary">Last Episode To Air: </span>${show.last_episode_to_air.name}</li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
           </ul>
           <h4>Production Companies</h4>
-          <div class="list-group">${movie.production_companies.map(
+          <div class="list-group">${show.production_companies.map(
             (company) => `
-          <span>${company.name}</span>`
+          <span>${show.name}</span>`
           ).join(', ')}</div>
         </div>
         `;
@@ -288,7 +282,7 @@ function init() {
       break;
 
     case '/flixx-app/shows.html':
-    case '/shows.html':
+    case '/shows.html': 
       displayPopularShows();
       break;
     
@@ -298,7 +292,6 @@ function init() {
 
     case '/tv-details.html':
       displayShowDetails();
-      console.log('TV Details');
       break;
 
     case '/search.html':
