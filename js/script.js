06adc8539f6957c4b8c2bc4ebfa00aea422cecc1
Alexeencokirill <97600028+Alexeencokirill@users.noreply.github.com>
2023-10-24 10:40:36 +0200

    search01

--- js/script.js
+++ js/script.js
@@ -270,7 +270,35 @@ async function search() {
 }
 
 function displaySearchResults(results) {
-  
+  results.forEach((result) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="${global.search.type}-details.html?id=${result.id}">
+            ${
+              result.poster_path
+                ? `<img
+                src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                class="card-img-top"
+                alt="${global.search.type === 'movie' ? result.title : result.name}"
+              />`
+                : `
+              <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${global.search.type === 'movie' ? result.title : result.name}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${global.search.type === 'movie' ? result.release_date : result.first_air_date}</small>
+            </p>
+          </div>`;
+
+    document.querySelector('#search-results').appendChild(div);
+  });
 }
 
 // Display Slider Movies
